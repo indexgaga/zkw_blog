599bb5502e048038534d2353e0ff2b41d7df0d23
zkw <415477041@qq.com>
2019-05-27 15:18:16 +0800

    项目基本架构提交

---
+++ src/main/java/cn/zkw/realm/cre/CustomerCredentialsMatcher.java
@@ -0,0 +1,19 @@
+package cn.zkw.realm.cre;
+
+import cn.zkw.util.encrypt.MyPasswordEncrypt;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.credential.SimpleCredentialsMatcher;
+
+
+
+public class CustomerCredentialsMatcher extends SimpleCredentialsMatcher {
+	@Override
+	public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
+		// 取得原始的输入数据信息
+		Object tokenCredentials = MyPasswordEncrypt.encryptPassword(super.toString(token.getCredentials())).getBytes();
+		// 取得认证数据库中的数据
+		Object accountCredentials = super.getCredentials(info) ;
+		return super.equals(tokenCredentials, accountCredentials); 
+	}
+}
---
+++ src/main/webapp/model.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: My
+  Date: 2019/5/26
+  Time: 15:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%
+    String path = request.getContextPath();      //上下文路径，/mldn
+    //请求方式                    服务器名，地址                          端口
+    String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
+<html>
+<head>
+    <base href="<%=basePath%>">
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
